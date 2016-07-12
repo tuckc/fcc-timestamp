55390c0695da8df2e97786068a3c45c08e08b03d
Colton <coltontuck94@gmail.com>
2016-07-12 19:27:53 +0000

    Converts date/timestamp to object sent to server as JSON

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const moment = require('moment');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send("Enter a date or unix timestamp");
+});
+
+app.get('/*', (req, res) => {
+  var date;
+  var obj = { 
+    unix: null,
+    natural: null
+  };
+  if(!(/[a-z]+/gi).test(req.params[0])) {
+    date = moment.unix(req.params[0]);
+    if(date.isValid()) {
+      obj.natural = date.format("MMMM D, YYYY");
+      obj.unix = req.params[0];
+    }
+  } else {
+    date = moment(req.params[0], ["MMMM D, YYYY"]);
+    if(date.isValid()) {
+      obj.natural = date.format("MMMM D, YYYY");
+      obj.unix = date.format("X");
+    }
+  }
+  res.json(obj);
 });
 
 app.listen(8080, function () {
